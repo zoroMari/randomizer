6441556033d8b9170ab8e50206c4c1a60dace262
Mariia Moroz <mashanya.kov@gmail.com>
2022-12-13 21:01:51 +0100

    added structure of main blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { TitleComponent } from './title/title.component';
+import { WordGeneratedComponent } from './word-generated/word-generated.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    TitleComponent,
+    WordGeneratedComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/word-generated/word-generated.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-word-generated',
+  templateUrl: './word-generated.component.html',
+  styleUrls: ['./word-generated.component.sass']
+})
+export class WordGeneratedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
