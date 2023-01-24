b22901b541a931356e9ca5bac3ba02ec27de47d7
Mariia Moroz <mashanya.kov@gmail.com>
2023-01-24 14:20:32 +0100

    combined all conditions in one cycle

--- src/app/pages/naming/naming-component.ts
+++ src/app/pages/naming/naming-component.ts
@@ -107,92 +107,62 @@ export class NamingComponent implements OnInit, OnDestroy {
     word.push(...options.start, ...options.includes, ...options.ends);
 
     const maxGeneratedLength = options.length - word.length;
+    const generatedPart = [];
+    const lastIndexOfPartBeforeInclude = Math.floor(maxGeneratedLength / 2) - 1;
+    const firstIndexofPartAfterInclude = Math.floor(maxGeneratedLength / 2);
 
-    if (options.includes.length === 0) {
-      const generatedPart = [];
+    while (generatedPart.length < maxGeneratedLength) {
+      let letter: string = this._filterMethodService.getRandomItemFromArray(options.lettersSelected);
 
-      while (generatedPart.length < maxGeneratedLength) {
-        let letter: string = this._filterMethodService.getRandomItemFromArray(options.lettersSelected);
-
-        if (options.identicalLetters === false) generatedPart.push(letter);
-        else {
-          if (
-            generatedPart.length === 0 &&
-            start.length > 0 &&
-            start[start.length - 1].toLowerCase() === letter.toLowerCase()
-          ) continue;
-
-          else if (
-            generatedPart.length > 0 &&
-            generatedPart[generatedPart.length - 1].toLowerCase() === letter.toLowerCase()
-          ) continue;
-
-          else if (
-            generatedPart.length === maxGeneratedLength - 1 &&
-            end.length > 0 &&
-            end[0].toLowerCase() === letter.toLowerCase()
-          ) continue;
-
-          else generatedPart.push(letter);
-        }
-      }
-
-      mediumPart = [...generatedPart];
-
-    } else {
-      const generatedPart = [];
-      const lastIndexOfPartBeforeInclude = Math.floor(maxGeneratedLength / 2) - 1;
-      const firstIndexofPartAfterInclude = Math.floor(maxGeneratedLength / 2);
-
-      console.log(' lastIndexOfPartBeforeInclude>>>', lastIndexOfPartBeforeInclude);
-      console.log(' firstIndexofPartAfterInclude>>>', firstIndexofPartAfterInclude);
-
-
-      while (generatedPart.length < maxGeneratedLength) {
-        let letter: string = this._filterMethodService.getRandomItemFromArray(options.lettersSelected);
-
-
-        if (options.identicalLetters === false) generatedPart.push(letter);
-        else {
-          if (
-            generatedPart.length === 0 &&
-            start.length > 0 &&
+      if (options.identicalLetters === false) generatedPart.push(letter);
+      else {
+        if (
+          generatedPart.length === 0 &&
+          (
             (
-              start[start.length - 1].toLowerCase() === letter.toLowerCase() ||
+              start.length > 0 &&
+              start[start.length - 1].toLowerCase() === letter.toLowerCase()
+            ) ||
+            (
+              include.length > 0 &&
               include[0].toLowerCase() === letter.toLowerCase()
             )
-          ) continue;
+          )
+        ) continue;
 
-          else if (
-            generatedPart.length > 0 &&
-            generatedPart[generatedPart.length - 1].toLowerCase() === letter.toLowerCase()
-          ) continue;
+        else if (
+          generatedPart.length > 0 &&
+          generatedPart[generatedPart.length - 1].toLowerCase() === letter.toLowerCase()
+        ) continue;
 
-          else if (
-            generatedPart.length === lastIndexOfPartBeforeInclude &&
-            letter.toLowerCase() === include[0].toLowerCase()
-          ) continue;
+        else if (
+          include.length > 0 &&
+          generatedPart.length === lastIndexOfPartBeforeInclude &&
+          letter.toLowerCase() === include[0].toLowerCase()
+        ) continue;
 
-          else if (
-            generatedPart.length === maxGeneratedLength - 1 &&
-            end.length > 0 &&
-            end[0].toLowerCase() === letter.toLowerCase()
-          ) continue;
+        else if (
+          generatedPart.length === maxGeneratedLength - 1 &&
+          end.length > 0 &&
+          end[0].toLowerCase() === letter.toLowerCase()
+        ) continue;
 
-          else if (
-            generatedPart.length === firstIndexofPartAfterInclude &&
-            letter.toLowerCase() === include[include.length - 1].toLowerCase()
-          ) continue;
+        else if (
+          include.length > 0 &&
+          generatedPart.length === firstIndexofPartAfterInclude &&
+          letter.toLowerCase() === include[include.length - 1].toLowerCase()
+        ) continue;
 
-          else generatedPart.push(letter);
+        else {
+          generatedPart.push(letter);
         }
       }
-
-      const firstPart = generatedPart.slice(0, lastIndexOfPartBeforeInclude + 1);
-      const secondPart = generatedPart.slice(firstIndexofPartAfterInclude);
-
-      mediumPart = [...firstPart, ...include, ...secondPart];
     }
+
+    const firstPart = generatedPart.slice(0, lastIndexOfPartBeforeInclude + 1);
+    const secondPart = generatedPart.slice(firstIndexofPartAfterInclude);
+
+    mediumPart = [...firstPart, ...include, ...secondPart];
 
     const newWord = [...start, ...mediumPart, ...end];
     this.wordGenerated = this._filterMethodService.addStyleToWord(newWord.join(''), options.style);
