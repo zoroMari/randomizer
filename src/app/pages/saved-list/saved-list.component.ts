e54a740321f475f199b252e2c9580cbef344f8ca
Mariia Moroz <mashanya.kov@gmail.com>
2023-01-26 17:08:56 +0100

    added downloading of savedList

--- src/app/pages/saved-list/saved-list.component.ts
+++ src/app/pages/saved-list/saved-list.component.ts
@@ -9,14 +9,14 @@ import { SavedService } from './saved-list.service';
   styleUrls: ['./saved-list.component.sass']
 })
 export class SavedListComponent implements OnInit {
-  constructor(private _savedService: SavedService, private _route: ActivatedRoute) { }
+  constructor(private _savedService: SavedService, public route: ActivatedRoute) { }
 
   public title: string = Titles.saved;
   public savedList!: string[];
   public noSaved: boolean = false;
 
   ngOnInit(): void {
-    this._route.params.subscribe(
+    this.route.params.subscribe(
       (param: Params) => {
         this.title = `${Titles.saved} (${param['title']})`;
         this._savedService.fetchSavedList(param['title']);
@@ -34,12 +34,24 @@ export class SavedListComponent implements OnInit {
     )
   }
 
+  public onDownload(){
+    this._download(this.savedList, "saved-list.txt");
+  }
+
   public handleDeleteWord(word: string): void {
     this._savedService.deleteWord(word);
   }
 
   public handleDeleteAllWords(): void {
     this._savedService.deleteAllWords();
+  }
+
+  private _download(content: string [], fileName: string) {
+    const a = document.createElement("a");
+    const file = new Blob([`${content}`], { type: 'text/plain' });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
   }
 
 }
