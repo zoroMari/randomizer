64d73d68fb9e0726a5ccc019f165fbb2182c8be5
Mariia Moroz <mashanya.kov@gmail.com>
2023-01-27 16:42:03 +0100

    added variables for generating conditions

--- src/app/pages/naming/naming-component.ts
+++ src/app/pages/naming/naming-component.ts
@@ -121,43 +121,43 @@ export class NamingComponent implements OnInit, OnDestroy {
     while (generatedPart.length < maxGeneratedLength) {
       let letter: string = this._filterMethodService.getRandomItemFromArray(options.lettersSelected);
 
-      if (options.identicalLetters === false || this.form.controls['identicalLetters'].disabled) generatedPart.push(letter);
+      const canBeIdenticalLetters = options.identicalLetters === false || this.form.controls['identicalLetters'].disabled;
+      const firstGeneratedLetterEqualsStartLastLetterOrIncludeFirstLetter =
+        generatedPart.length === 0
+        && (
+          (
+            start.length > 0
+            && start[start.length - 1].toLowerCase() === letter.toLowerCase()
+          )
+          || (
+            include.length > 0 &&
+            include[0].toLowerCase() === letter.toLowerCase()
+          )
+        );
+      const newGeneratedLetterEqualsPreviousGeneratedLetter =
+        generatedPart.length > 0
+        && generatedPart[generatedPart.length - 1].toLowerCase() === letter.toLowerCase();
+      const lastLetterOfFirstGeneratedPartEqualsFirstIncludeLetter =
+        include.length > 0
+        && generatedPart.length === lastIndexOfPartBeforeInclude
+        && letter.toLowerCase() === include[0].toLowerCase();
+      const firstLetterOfSecondGeneratedPartEqualsLastIncludeLetter =
+        include.length > 0
+        && generatedPart.length === firstIndexofPartAfterInclude
+        && letter.toLowerCase() === include[include.length - 1].toLowerCase();
+      const lastLetterOfSecondGeneratedPartEqualsFirstEndLetter =
+        generatedPart.length === maxGeneratedLength - 1
+        && end.length > 0
+        && end[0].toLowerCase() === letter.toLowerCase();
+
+      if (canBeIdenticalLetters) generatedPart.push(letter);
       else {
         if (
-          generatedPart.length === 0 &&
-          (
-            (
-              start.length > 0 &&
-              start[start.length - 1].toLowerCase() === letter.toLowerCase()
-            ) ||
-            (
-              include.length > 0 &&
-              include[0].toLowerCase() === letter.toLowerCase()
-            )
-          )
-        ) continue;
-
-        else if (
-          generatedPart.length > 0 &&
-          generatedPart[generatedPart.length - 1].toLowerCase() === letter.toLowerCase()
-        ) continue;
-
-        else if (
-          include.length > 0 &&
-          generatedPart.length === lastIndexOfPartBeforeInclude &&
-          letter.toLowerCase() === include[0].toLowerCase()
-        ) continue;
-
-        else if (
-          generatedPart.length === maxGeneratedLength - 1 &&
-          end.length > 0 &&
-          end[0].toLowerCase() === letter.toLowerCase()
-        ) continue;
-
-        else if (
-          include.length > 0 &&
-          generatedPart.length === firstIndexofPartAfterInclude &&
-          letter.toLowerCase() === include[include.length - 1].toLowerCase()
+          firstGeneratedLetterEqualsStartLastLetterOrIncludeFirstLetter
+          || newGeneratedLetterEqualsPreviousGeneratedLetter
+          || lastLetterOfFirstGeneratedPartEqualsFirstIncludeLetter
+          || firstLetterOfSecondGeneratedPartEqualsLastIncludeLetter
+          || lastLetterOfSecondGeneratedPartEqualsFirstEndLetter
         ) continue;
 
         else {
